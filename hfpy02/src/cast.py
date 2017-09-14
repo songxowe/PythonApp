e7c9e02bf37d724f4d671ddffd3a7e0e3d54300f
SONG <songxowe@hotmail.com>
2017-09-14 11:20:30 +0800

    Head First Python

--- hfpy02/src/cast.py
+++ hfpy02/src/cast.py
@@ -4,3 +4,8 @@ cast = ['Palin', 'Cleese', 'Idle', 'Jones', 'Gilliam', 'Chapman']
 
 # 命名空间限定函数名
 nester.print_lol(cast)
+
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+          ["Graham Chapman", ["Michael Palin", "John Cleese",
+                              "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+nester.print_lol(movies, True, 0)
--- hfpy02/src/nester.py
+++ hfpy02/src/nester.py
@@ -12,3 +12,30 @@ def print_lol(the_list):
             print_lol(each_item)
         else:
             print(each_item)
+
+
+def print_lol(a_list, level=0):
+    """Prints each item in a list, recursively descending
+       into nested lists (if necessary)."""
+
+    for each_item in a_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level + 1)
+        else:
+            for l in range(level):
+                print("\t", end='')
+            print(each_item)
+
+
+def print_lol(a_list, indent=False, level=0):
+    """Prints each item in a list, recursively descending
+       into nested lists (if necessary)."""
+
+    for each_item in a_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level + 1)
+        else:
+            if indent:
+                for l in range(level):
+                    print("\t", end='')
+            print(each_item)
