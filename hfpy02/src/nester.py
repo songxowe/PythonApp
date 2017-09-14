e64532db359583f5f6021e6b5ec51316eabf5642
SONG <songxowe@hotmail.com>
2017-09-14 10:56:18 +0800

    Head First Python

--- hfpy02/src/nester.py
+++ hfpy02/src/nester.py
@@ -1,11 +1,11 @@
 """This is the "nester.py" module and it provides one function called print_lol()
-   Which prints lists that may or may not include nested lists."""
+   which prints lists that may or may not include nested lists."""
 
 
 def print_lol(the_list):
-    """This function takes one positional argument called "the_list",
-       which is any Python list (of - possibly - nested list). Each data item
-       in the provided list is (recursively) printed to the screen on it's own line."""
+    """This function takes one positional argument called "the_list", which
+        is any Python list (of - possibly - nested lists). Each data item in the
+        provided list is (recursively) printed to the screen on it’s own line."""
 
     for each_item in the_list:
         if isinstance(each_item, list):
