92933fbc0f43fc457616558989dcb7893983a0f7
SONG <songxowe@hotmail.com>
2017-09-19 16:08:24 +0800

    Head First Python

---
+++ hfpy04/src/sports1.py
@@ -0,0 +1,38 @@
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return (time_string)
+    (mins, secs) = time_string.split(splitter)
+    return (mins + '.' + secs)
+
+
+with open('james.txt') as jaf:
+    data = jaf.readline()
+james = data.strip().split(',')
+
+with open('julie.txt') as juf:
+    data = juf.readline()
+julie = data.strip().split(',')
+
+with open('mikey.txt') as mif:
+    data = mif.readline()
+mikey = data.strip().split(',')
+
+with open('sarah.txt') as saf:
+    data = saf.readline()
+sarah = data.strip().split(',')
+
+# 列表推导 list comprehension: 为了减少将一个列表转化为另一个列表时所需编写的代码量
+print(sorted([sanitize(t) for t in james]))
+print(sorted([sanitize(t) for t in julie]))
+print(sorted([sanitize(t) for t in mikey]))
+print(sorted([sanitize(t) for t in sarah]))
+
+# 前三次最好成绩
+print(sorted(set([sanitize(t) for t in james]))[0:3])
+print(sorted(set([sanitize(t) for t in julie]))[0:3])
+print(sorted(set([sanitize(t) for t in mikey]))[0:3])
+print(sorted(set([sanitize(t) for t in sarah]))[0:3])
