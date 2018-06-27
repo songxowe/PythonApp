156b52ad6d20326b03643a7cf2323c3bf25365d2
SONG <songxowe@hotmail.com>
2018-06-27 20:49:45 +0800

    Why is python for big data! No Java!

--- ch02base/src/Hello.py
+++ ch02base/src/Hello.py
@@ -1,9 +1,10 @@
 # 注意到 print 是一个函数
 print('hello world')
 
+# 多行注释
 '''这是一段多行字符串。这是它的第一行。
 This is the second line.
-"What's your name?," I asked.
+"What's your name?", I asked.
 He said "Bond, James Bond."
 '''
 
